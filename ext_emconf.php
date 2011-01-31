efbfa3ce0eb22764ac54a6a9d9d7e457dcc379d6
el_curtismo <el_curtismo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-31 22:16:41 +0000

    Release 1.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cwmobileredirect/trunk@42879 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cwmobileredirect".
 #
-# Auto generated 19-01-2011 22:24
+# Auto generated 31-01-2011 23:14
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Your all-in-one mobile device detection and redirection solution! Detects mobile browsers and redirects to other Typo3 sites in your setup (most likely optimized for mobiles). Allows to easily switch back to the normal version, with Cookie support to remember the users choice. The browser detection can be access via TypoScript or in your own extension.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.1.1',
+	'version' => '1.2.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"9e20";s:29:"class.tx_cwmobileredirect.php";s:4:"a582";s:21:"ext_conf_template.txt";s:4:"05b4";s:12:"ext_icon.gif";s:4:"4904";s:17:"ext_localconf.php";s:4:"51fd";s:14:"ext_tables.php";s:4:"96e4";s:14:"doc/manual.sxw";s:4:"cf6f";s:33:"static/cwmobileredirect/setup.txt";s:4:"0f73";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"bfd1";s:29:"class.tx_cwmobileredirect.php";s:4:"be7b";s:21:"ext_conf_template.txt";s:4:"a62c";s:12:"ext_icon.gif";s:4:"4904";s:17:"ext_localconf.php";s:4:"51fd";s:14:"doc/manual.sxw";s:4:"8bc0";}',
 	'suggests' => array(
 	),
 );
